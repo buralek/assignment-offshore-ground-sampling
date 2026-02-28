f81b7780e754ab80e66cde6821d99e10fc127d01
Aleksandr Burashnikov <buralek91@gmail.com>
2026-02-28 21:22:04 +0100

    add docker and init sql

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO location (id, name, zone_id, latitude, longitude) VALUES
+    (RANDOM_UUID(), 'North Sea Platform Alpha', 'Europe/Amsterdam', 57.1234, 2.4568),
+    (RANDOM_UUID(), 'North Sea Platform Beta',  'Europe/London',    57.8901, 1.2345),
+    (RANDOM_UUID(), 'Gulf of Mexico Platform A','America/Chicago',  28.9123, -90.1234),
+    (RANDOM_UUID(), 'Norwegian Continental Shelf', 'Europe/Oslo',   60.4720, 5.3241);
